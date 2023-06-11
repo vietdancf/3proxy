99298c602bf52cced551120500d4b2003a5863d1
vietdancf <111975427+vietdancf@users.noreply.github.com>
2023-06-11 14:31:15 +0700

    Update nopass.sh

--- nopass.sh
+++ nopass.sh
@@ -88,8 +88,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=22000
-LAST_PORT=23000
+FIRST_PORT=42000
+LAST_PORT=43000
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
