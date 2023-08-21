1420f2b7da9ec4e7844fb36a33420f2a93693fa5
vietdancf <111975427+vietdancf@users.noreply.github.com>
2023-08-21 23:03:02 +0700

    Update nopass.sh

--- nopass.sh
+++ nopass.sh
@@ -88,8 +88,18 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=28701
-LAST_PORT=29700
+while :; do
+  read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
+  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
+    echo "OK! Valid number"
+    break
+  else
+    echo "Number out of range, try again"
+  fi
+done
+LAST_PORT=$(($FIRST_PORT + 2000))
+echo "LAST_PORT is $LAST_PORT. Continue..."
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
